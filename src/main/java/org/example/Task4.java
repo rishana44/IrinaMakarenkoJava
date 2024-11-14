7d4c9ddaeaa16299ec4bd1186ad3eac41fe96b56
IrinaMakarenko <94065919+rishana44@users.noreply.github.com>
2024-11-14 15:41:45 +0300

    Update Task4.java

--- src/main/java/org/example/Task4.java
+++ src/main/java/org/example/Task4.java
@@ -1,2 +1,11 @@
-package org.example;public class Task4 {
+
+public class Task4 {
+// Answer the questions
+// Given bracket sequence: [((())()(())]]
+// Can this sequence be considered correct?
+// If the answer to the previous question is “no”, then what needs to be changed in it to make it correct?
 }
+//Answer:
+//No. Because according to one of the basic principles of encapsulation, code that refers to a method, class, loop,
+// or condition must be between parentheses. Parentheses round, square, curly, charcoal all of them must open and close. The code will work within these brackets.The correct parenthesis sequence is: [[((())()(()))]]
+// P.S. Excluding access modifiers
