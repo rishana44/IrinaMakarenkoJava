127d14be2d1735ce90dc3d75790482baf066940e
IrinaMakarenko <94065919+rishana44@users.noreply.github.com>
2024-11-14 15:40:30 +0300

    Update Task1.java

--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -1,2 +1,17 @@
-package org.example;public class Task1 {
+
+import java.util.Scanner;
+//Task1: Make up an algorithm. If the entered number is greater than 7, then print “Hello”
+public class Task1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = scanner.nextInt();
+
+        if (number > 7) {
+            System.out.println("Hello");
+        }
+        scanner.close();
+    }
 }
