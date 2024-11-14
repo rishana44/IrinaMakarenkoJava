4e8f608cf1365a395ef2607f096c0d763eb38805
IrinaMakarenko <94065919+rishana44@users.noreply.github.com>
2024-11-14 15:40:59 +0300

    Update Task2.java

--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -1,2 +1,21 @@
-package org.example;public class Task2 {
+import java.util.Scanner;
+//Task1: Make up an algorithm. If the entered name matches “John”, then output “Hello, John”, if not, then output "There is no such name"
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+
+        String name = scanner.nextLine();
+
+        if (name.equals("John")) {
+
+            System.out.println("Hello, John");
+        } else {
+
+            System.out.println("There is no such name");
+        }
+
+        scanner.close();
+    }
 }
